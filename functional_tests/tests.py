c24b827f11b13656153781fde7aec9dd0fe91ebe
Latif <laateef@outlook.com>
2019-07-18 22:52:07 +0300

    First Commit: First FT and basic Django config

---
+++ functional_tests/tests.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from django.test import TestCase
+
+class MainPageTest(TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_title_is_correct(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('Django', self.browser.title)
