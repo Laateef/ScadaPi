a5ba3f933746f20289a2821e7b6dfcd163736eca
Laateef <laateef@outlook.com>
2019-07-22 13:48:50 +0300

    refactor raspberrypi

--- raspberrypi/tests.py
+++ raspberrypi/tests.py
@@ -1,43 +1,40 @@
 from unittest import TestCase
 from unittest.mock import patch
+from unittest import mock
 
 import smbus2
 
-from . import ControlApi, Enums
+
+from raspberrypi import interfaces
+from raspberrypi import enums
 
 class AnalogInputModuleTest(TestCase):
 
 	@patch('smbus2.SMBus')
 	def setUp(self, SMBusMock):
 		self.bus = SMBusMock	
-		self.adc = ControlApi.ADC(self.bus)
+		self.adc = interfaces.ADC(self.bus)
 	
 	def test_readChannel_configures_channel_0_for_reading(self):
 		self.adc.readChannel(0)
 
-		self.assertTrue(self.bus.write_i2c_block_data.called)
-		(i2c_address, register_address, data_array), kwargs = self.bus.write_i2c_block_data.call_args
-		self.assertEqual(i2c_address, Enums.ADC_I2C_ADDRESS_1)
-		self.assertEqual(register_address, Enums.ADC_REG_CONFIGURATION)
-		self.assertEqual(data_array, [Enums.AIN_CMN_CFG_HByte | Enums.ADC_CFG_MUX_AIN_0, Enums.AIN_CMN_CFG_LByte])
+		self.bus.write_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_1, 
+								enums.ADC_REG_CONFIGURATION, 
+								[enums.AIN_CMN_CFG_HByte | enums.ADC_CFG_MUX_AIN_0, enums.AIN_CMN_CFG_LByte])
 
 	def test_readChannel_configures_channel_1_for_reading(self):
 		self.adc.readChannel(1)
 
-		self.assertTrue(self.bus.write_i2c_block_data.called)
-		(i2c_address, register_address, data_array), kwargs = self.bus.write_i2c_block_data.call_args
-		self.assertEqual(i2c_address, Enums.ADC_I2C_ADDRESS_1)
-		self.assertEqual(register_address, Enums.ADC_REG_CONFIGURATION)
-		self.assertEqual(data_array, [Enums.AIN_CMN_CFG_HByte | Enums.ADC_CFG_MUX_AIN_1, Enums.AIN_CMN_CFG_LByte])
+		self.bus.write_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_1, 
+								enums.ADC_REG_CONFIGURATION, 
+								[enums.AIN_CMN_CFG_HByte | enums.ADC_CFG_MUX_AIN_1, enums.AIN_CMN_CFG_LByte])
 
 	def test_readChannel_configures_channel_4_for_reading(self):
 		self.adc.readChannel(4)
 
-		self.assertTrue(self.bus.write_i2c_block_data.called)
-		(i2c_address, register_address, data_array), kwargs = self.bus.write_i2c_block_data.call_args
-		self.assertEqual(i2c_address, Enums.ADC_I2C_ADDRESS_2)
-		self.assertEqual(register_address, Enums.ADC_REG_CONFIGURATION)
-		self.assertEqual(data_array, [Enums.AIN_CMN_CFG_HByte | Enums.ADC_CFG_MUX_AIN_0, Enums.AIN_CMN_CFG_LByte])
+		self.bus.write_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_2, 
+								enums.ADC_REG_CONFIGURATION, 
+								[enums.AIN_CMN_CFG_HByte | enums.ADC_CFG_MUX_AIN_0, enums.AIN_CMN_CFG_LByte])
 
 	def test_readChannel_raises_exception_if_channel_index_is_out_of_range(self):
 		self.assertRaises(IndexError, self.adc.readChannel, -1)
@@ -46,52 +43,48 @@ class AnalogInputModuleTest(TestCase):
 	def test_readChannel_reads_channel_0(self):
 		self.adc.readChannel(0)
 
-		self.assertTrue(self.bus.read_i2c_block_data.called)
-		(i2c_address, register_address, data_array_size), kwargs = self.bus.read_i2c_block_data.call_args
-		self.assertEqual(i2c_address, Enums.ADC_I2C_ADDRESS_1)
-		self.assertEqual(register_address, Enums.ADC_REG_CONVERSION)
-		self.assertEqual(data_array_size, 2)
+		self.bus.read_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_1, enums.ADC_REG_CONVERSION, 2)
 
 	def test_readChannel_reads_channel_7(self):
 		self.adc.readChannel(7)
 
-		self.assertTrue(self.bus.read_i2c_block_data.called)
-		(i2c_address, register_address, data_array_size), kwargs = self.bus.read_i2c_block_data.call_args
-		self.assertEqual(i2c_address, Enums.ADC_I2C_ADDRESS_2)
-		self.assertEqual(register_address, Enums.ADC_REG_CONVERSION)
-		self.assertEqual(data_array_size, 2)
+		self.bus.read_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_2, enums.ADC_REG_CONVERSION, 2)
 
 	@patch('time.sleep')
 	def test_readChannel_waits_until_conversion_is_done(self, time_sleep_mock):
 		self.adc.readChannel(6)
 
 		self.assertTrue(self.bus.write_i2c_block_data.called)
-		self.assertTrue(time_sleep_mock.called)
-		args, kwargs = time_sleep_mock.call_args
-		# To be on the safe side, 1 msec is added to the sleep function
-		self.assertAlmostEqual(args[0], 1.0 / Enums.ADC_SAMPLE_PER_SECOND + 0.001) 
+		# To be on the safe side, readChannel should wait 1 msec more after the config is written.
+		time_sleep_mock.assert_called_with(1.0 / enums.ADC_SAMPLE_PER_SECOND + 0.001)
 		self.assertTrue(self.bus.read_i2c_block_data.called)
 
-	@patch('smbus2.SMBus.read_i2c_block_data', return_value = [0x00, 0x00])
-	def test_readChannel_returns_integer(self, read_i2c_block_data_mock):
-		self.bus.read_i2c_block_data = read_i2c_block_data_mock
+	def test_readChannel_returns_zero_integer_for_zero(self):
+		self.bus.read_i2c_block_data = mock.Mock('self.bus.read_i2c_block_data', return_value = [0x00, 0x00])
 
 		adc_value = self.adc.readChannel(7)
 
 		self.assertEqual(adc_value, 0)
 		self.assertEqual(type(adc_value), int)
 
-	@patch('smbus2.SMBus.read_i2c_block_data')
-	def test_readChannel_returns_two_complement_conversion(self, read_i2c_block_data_mock):
-		self.bus.read_i2c_block_data = read_i2c_block_data_mock
+	def test_readChannel_returns_two_complement_conversion_for_negatives(self):
+		self.bus.read_i2c_block_data.return_value = [0x80, 0x00]
 
-		read_i2c_block_data_mock.return_value = [0x80, 0x00]
 		adc_value = self.adc.readChannel(7)
+
 		self.assertEqual(adc_value, -32768)
 
-		read_i2c_block_data_mock.return_value = [0x7F, 0xFF]
+	def test_readChannel_returns_two_complement_conversion_for_positives(self):
+		self.bus.read_i2c_block_data.return_value = [0x7F, 0xFF]
+
 		adc_value = self.adc.readChannel(7)
+
 		self.assertEqual(adc_value, 32767)
+
+
+
+
+
 
 
 
