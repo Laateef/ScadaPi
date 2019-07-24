9f780270b19ab427f783bd6cd5314f030d7c9dd8
Laateef <laateef@outlook.com>
2019-07-24 09:00:58 +0300

    Refactor raspberrypi.tests

--- raspberrypi/tests.py
+++ raspberrypi/tests.py
@@ -1,48 +1,40 @@
 from unittest import TestCase
 from unittest.mock import patch
-from unittest import mock
-
-import smbus2
-
-from gpiozero import LED
 
 from raspberrypi import enums
 from raspberrypi import interfaces
 
+
 class AnalogInputModuleTest(TestCase):
 
-	@patch('smbus2.SMBus')
+	@patch('raspberrypi.interfaces.smbus2.SMBus', autospec=True)
 	def setUp(self, busMock):
-	
 		self.adc = interfaces.ADC()
-		self.bus = self.adc.bus
+		self.bus = busMock.return_value
 
-	def test_ADC_closes_the_bus_when_deleted(self):
-		another_adc = self.adc
-		another_bus = another_adc.bus
-		
-		another_adc.__del__()
+	def test_ADC_closes_the_bus_when_deleted(self):	
+		self.adc.__del__()
 
-		another_bus.close.assert_called()
+		self.bus.close.assert_called_once()
 	
 	def test_readChannel_configures_channel_0_for_reading(self):
 		self.adc.readChannel(0)
 
-		self.bus.write_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_1, 
+		self.bus.write_i2c_block_data.assert_called_once_with(enums.ADC_I2C_ADDRESS_1, 
 								enums.ADC_REG_CONFIGURATION, 
 								[enums.AIN_CMN_CFG_HByte | enums.ADC_CFG_MUX_AIN_0, enums.AIN_CMN_CFG_LByte])
 
 	def test_readChannel_configures_channel_1_for_reading(self):
 		self.adc.readChannel(1)
 
-		self.bus.write_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_1, 
+		self.bus.write_i2c_block_data.assert_called_once_with(enums.ADC_I2C_ADDRESS_1, 
 								enums.ADC_REG_CONFIGURATION, 
 								[enums.AIN_CMN_CFG_HByte | enums.ADC_CFG_MUX_AIN_1, enums.AIN_CMN_CFG_LByte])
 
-	def test_readChannel_configures_channel_4_for_reading(self):
+	def test_readChannel_configures_channel_4_for_reading(self):	
 		self.adc.readChannel(4)
 
-		self.bus.write_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_2, 
+		self.bus.write_i2c_block_data.assert_called_once_with(enums.ADC_I2C_ADDRESS_2, 
 								enums.ADC_REG_CONFIGURATION, 
 								[enums.AIN_CMN_CFG_HByte | enums.ADC_CFG_MUX_AIN_0, enums.AIN_CMN_CFG_LByte])
 
@@ -53,24 +45,24 @@ class AnalogInputModuleTest(TestCase):
 	def test_readChannel_reads_channel_0(self):
 		self.adc.readChannel(0)
 
-		self.bus.read_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_1, enums.ADC_REG_CONVERSION, 2)
+		self.bus.read_i2c_block_data.assert_called_once_with(enums.ADC_I2C_ADDRESS_1, enums.ADC_REG_CONVERSION, 2)
 
 	def test_readChannel_reads_channel_7(self):
 		self.adc.readChannel(7)
 
-		self.bus.read_i2c_block_data.assert_called_with(enums.ADC_I2C_ADDRESS_2, enums.ADC_REG_CONVERSION, 2)
+		self.bus.read_i2c_block_data.assert_called_once_with(enums.ADC_I2C_ADDRESS_2, enums.ADC_REG_CONVERSION, 2)
 
-	@patch('time.sleep')
+	@patch('raspberrypi.interfaces.time.sleep', autospec=True)
 	def test_readChannel_waits_until_conversion_is_done(self, time_sleep_mock):
 		self.adc.readChannel(6)
 
-		self.assertTrue(self.bus.write_i2c_block_data.called)
+		self.bus.write_i2c_block_data.assert_called_once()
 		# To be on the safe side, readChannel should wait 1 msec more after the config is written.
-		time_sleep_mock.assert_called_with(1.0 / enums.ADC_SAMPLE_PER_SECOND + 0.001)
-		self.assertTrue(self.bus.read_i2c_block_data.called)
+		time_sleep_mock.assert_called_once_with(1.0 / enums.ADC_SAMPLE_PER_SECOND + 0.001)
+		self.bus.read_i2c_block_data.assert_called_once()
 
 	def test_readChannel_returns_zero_integer_for_zero(self):
-		self.bus.read_i2c_block_data = mock.Mock('self.bus.read_i2c_block_data', return_value = [0x00, 0x00])
+		self.bus.read_i2c_block_data.return_value = [0x00, 0x00]
 
 		adc_value = self.adc.readChannel(7)
 
@@ -92,22 +84,20 @@ class AnalogInputModuleTest(TestCase):
 		self.assertEqual(adc_value, 32767)
 
 
-@patch('gpiozero.LED')
+@patch('raspberrypi.interfaces.gpiozero.LED', autospec=True)
 class DigitalOutputModuleTest(TestCase):
-
-	def setUp(self):
-		self.gpio = interfaces.DO()	
 	
-	def test_turns_relay_0_off(self, ledMock):
-		self.gpio.off(0)
+	def test_turns_channel_0_off(self, gpioMock):
+		interfaces.DO().off(0)
 
-		ledMock.assert_called_with(0)
-		ledMock.on.assert_called
-	def test_turns_channel_0_on(self, ledMock):
-		self.gpio.on(0)
+		gpioMock.assert_called_once_with(0)
+		gpioMock.return_value.off.assert_called_once()
+		
+	def test_turns_channel_0_on(self, gpioMock):
+		interfaces.DO().on(0)
 
-		ledMock.assert_called_with(0)
-		ledMock.off.assert_called
+		gpioMock.assert_called_once_with(0)
+		gpioMock.return_value.on.assert_called_once()
 		
 
 
