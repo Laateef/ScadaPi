8fb29cdd7fc244ee23eac472e235cc802aec2c14
Laateef <laateef@outlook.com>
2019-08-21 01:25:48 +0300

    Refactor populate functions in base.js

--- mainapp/static/js/base.js
+++ mainapp/static/js/base.js
@@ -16,38 +16,25 @@ var base = {
 			}
 		});
 	},
-	populate_heater_array: function() {
+	populate_generic_device_array: function(device_type) {
 		$.ajax({
-			url: '/api/heater/',
+			url: '/api/' + device_type + '/',
 			method: 'GET',
 			success: function(response) {
 				for (var i = 0; i < response.length; ++i) {
-					document.getElementById('h' + response[i].id).innerHTML = response[i].state;
+					document.getElementById(device_type[0] + response[i].id).innerHTML = response[i].state;
 				}
 			}
 		});
+	},	
+	populate_heater_array: function() {
+		this.populate_generic_device_array('heater');
 	},
 	populate_pump_array: function() {
-		$.ajax({
-			url: '/api/pump/',
-			method: 'GET',
-			success: function(response) {
-				for (var i = 0; i < response.length; ++i) {
-					document.getElementById('p' + response[i].id).innerHTML = response[i].state;
-				}
-			}
-		});
+		this.populate_generic_device_array('pump');
 	},
 	populate_valve_array: function() {
-		$.ajax({
-			url: '/api/valve/',
-			method: 'GET',
-			success: function(response) {
-				for (var i = 0; i < response.length; ++i) {
-					document.getElementById('v' + response[i].id).innerHTML = response[i].state;
-				}
-			}
-		});
+		this.populate_generic_device_array('valve');
 	},
 
 };
