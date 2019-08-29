87ee57e72e9c6b5507cc5250b0b7c6bc8d02d334
Laateef <laateef@outlook.com>
2019-08-29 11:47:34 +0300

    Save temperature values into database during experiment

--- raspberrypi/automation.py
+++ raspberrypi/automation.py
@@ -1,22 +1,43 @@
-from raspberrypi.devices import Heater
-from raspberrypi.devices import Pump
-from raspberrypi.devices import Valve
-from raspberrypi.devices import Thermistor
+from .devices import Heater, Pump, Valve, Thermistor
+
+from . import enums
+
+from mainapp.models import Experiment, Temperature
 
 from threading import Thread
 
 import time
+
+# flag to indicate automation loop state 
+running = False
+
+# experiment object is created and saved for each automation session 
+experiment = None
 
 # regularly fetched thermistor temperature array
 
 temperature_list = []
 
 def update_temperature_list():
-	global temperature_list
+	global running, experiment, temperature_list
 
 	while True:
 		temperature_list = Thermistor.temperature_list()
-		time.sleep(1)
+
+		if running:
+			Temperature.objects.create(
+				experiment = experiment,
+				thermistor_1 = temperature_list[0],
+				thermistor_2 = temperature_list[1],
+				thermistor_3 = temperature_list[2],
+				thermistor_4 = temperature_list[3],
+				thermistor_5 = temperature_list[4],
+				thermistor_6 = temperature_list[5],
+				thermistor_7 = temperature_list[6],
+				thermistor_8 = temperature_list[7],
+			).save()
+
+		time.sleep(enums.TEMPERATURE_REFRESH_INTERVAL)
 
 Thread(target=update_temperature_list, daemon=True).start()
 
@@ -36,16 +57,18 @@ device_map = {
 	'valve_5': Valve(5)
 }
 
-# flag to indicate automation loop state 
-running = False
-
 def start():
-	global running
+	global running, experiment
 	running = True
+	
+	experiment = Experiment.objects.create()
+	experiment.save()
 
 def stop():
-	global running
+	global running, experiment
 	running = False
+
+	experiment.save()
 
 def toggle():
 	global running
--- raspberrypi/test_automation.py
+++ raspberrypi/test_automation.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 from unittest.mock import patch
-from unittest.mock import call
+
+from mainapp.models import Experiment, Temperature
+
+from . import enums
+
+import time
+import datetime
+
+from django.utils import timezone
 
 with patch('raspberrypi.devices.Thermistor', autospec=True) as thermistorMock:
 	temperature_list = [25.1, 26.2, 27.3, 28.4, 29.5, 30.6, 31.7, 32.8]
@@ -14,5 +22,33 @@ with patch('raspberrypi.devices.Thermistor', autospec=True) as thermistorMock:
 class AutomationTest(TestCase):
 
 	def test_fetch_temperature_list(self):
-		self.assertEqual(thermistorMock.temperature_list.call_count, 1)
+		self.assertNotEqual(thermistorMock.temperature_list.call_count, 0)
 		self.assertEqual(automation.temperature_list, temperature_list)
+
+	def test_save_temperature_list_in_database_only_during_experiment(self):
+		Temperature.objects.all().delete()
+
+		self.assertEqual(Temperature.objects.all().count(), 0)
+
+		automation.start()	
+		
+		time.sleep(enums.TEMPERATURE_REFRESH_INTERVAL + 0.1)
+
+		self.assertEqual(Temperature.objects.all().count(), 1)
+
+		automation.stop()	
+		
+		time.sleep(enums.TEMPERATURE_REFRESH_INTERVAL + 0.1)
+
+		self.assertEqual(Temperature.objects.all().count(), 1)
+
+	def test_experiment_start_and_end_date(self):
+		automation.start()
+
+		self.assertTrue(timezone.now() - automation.experiment.start_date < datetime.timedelta(milliseconds=500))	
+		
+		time.sleep(enums.TEMPERATURE_REFRESH_INTERVAL + 0.1)
+
+		automation.stop()	
+		
+		self.assertTrue(timezone.now() - automation.experiment.end_date < datetime.timedelta(milliseconds=500))	
