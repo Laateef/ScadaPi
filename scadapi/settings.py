8f19aeca7cee61093b9f05534f55670cfa6d39a7
Laateef <laateef@outlook.com>
2019-09-05 22:13:13 +0300

    Move SQLite database outside of main source tree

--- scadapi/settings.py
+++ scadapi/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'scadapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
